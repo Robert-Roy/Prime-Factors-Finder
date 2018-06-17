27583d5638e2705b65d055a1c49a9315bd534863
Robert-Roy <robertproy@live.com>
2018-06-17 14:37:53 -0500

    REFACTOR: Reorder lines to make code more intuitive. Add comments

--- src/FindPrimeFactors.php
+++ src/FindPrimeFactors.php
@@ -17,6 +17,7 @@ class FindPrimeFactors extends Command {
 
     public function execute(InputInterface $input, OutputInterface $output) {
         $inputNumber = $input->getArgument('Number');
+        // Fail on any non-integer input
         if (filter_var($inputNumber, FILTER_VALIDATE_INT) === false) {
             $output->writeln("<error>Input is not an integer. Please enter an integer</error>");
             exit(1);
@@ -28,20 +29,24 @@ class FindPrimeFactors extends Command {
     private function findFactors($inputNumbers) {
         $outputNumbers = $inputNumbers;
         for ($a = 0; $a < count($outputNumbers); $a++) {
+            //start trying to devide number by everything less than it except 1
             for ($i = 2; $i < $outputNumbers[$a]; $i++) {
+                // if number is divisible by tested number, and not equal to the tested number
                 if (($outputNumbers[$a] % $i) === 0 && $outputNumbers[$a] !== $i) {
-                    // cut $a value from array, push $input[$a] and $input[$a]/$i
+                    // cut $a value from array, push $i and $input[$a]/$i
                     $factorA = $outputNumbers[$a] / $i;
-                    $factorB = $i;array_splice($outputNumbers, $a, 1);
-                    $a++;
+                    $factorB = $i;
+                    array_splice($outputNumbers, $a, 1);
                     array_push($outputNumbers, $factorA, $factorB);
+                    // increase index of $a by one to compensate for incrased array length
+                    $a++;
                 }
             }
         }
         if($inputNumbers !== $outputNumbers){
             return $this->findFactors($outputNumbers);
         }
-        asort($outputNumbers);
+        asort($factorArray);
         return $outputNumbers;
     }
     
